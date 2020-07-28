3549f86788903a5b814beee853d7336653db830e
sammydowds <sammydowds1993@gmail.com>
2020-07-28 09:44:17 -0500

    Converting to functional component

--- src/MainComponent.js
+++ src/MainComponent.js
@@ -4,18 +4,18 @@ import HeatMap from './components/HeatMapComponent';
 function MainComponent() {
     const color = [255,165,0]; 
     const items = [
-        {'top': 'Project 1', 'bottom':'bottom', 'left': 'left', 'right': 'right', 'bottom': 'bottom', 'display': 2,'index': 8},
-        {'top': 'Project 2', 'bottom':'bottom', 'left': 'left', 'right': 'right', 'bottom': 'bottom', 'display': 9,'index': 1}, 
-        {'top': 'Project 3', 'bottom':'bottom', 'left': 'left', 'right': 'right', 'bottom': 'bottom', 'display': 4,'index': 6}, 
-        {'top': 'Project 4', 'bottom':'bottom', 'left': 'left', 'right': 'right', 'bottom': 'bottom', 'display': 1,'index': 10},
-        {'top': 'Project 5', 'bottom':'bottom', 'left': 'left', 'right': 'right', 'bottom': 'bottom', 'display': 3,'index': 3},
-        {'top': 'Project 6', 'bottom':'bottom', 'left': 'left', 'right': 'right', 'bottom': 'bottom', 'display': 8,'index': 2}, 
-        {'top': 'Project 7', 'bottom':'bottom', 'left': 'left', 'right': 'right', 'bottom': 'bottom', 'display': 6,'index': 4}, 
-        {'top': 'Project 8', 'bottom':'bottom', 'left': 'left', 'right': 'right', 'bottom': 'bottom', 'display': 2,'index': 8}, 
-        {'top': 'Project 9', 'bottom':'bottom', 'left': 'left', 'right': 'right', 'bottom': 'bottom', 'display': 9,'index': 1},
-        {'top': 'Project 10','bottom':'bottom', 'left': 'left', 'right': 'right' , 'bottom': 'bottom', 'display': 5,'index': 5}, 
-        {'top': 'Project 11','bottom':'bottom', 'left': 'left', 'right': 'right' , 'bottom': 'bottom', 'display': 7,'index': 3}, 
-        {'top': 'Project 12','bottom':'bottom', 'left': 'left', 'right': 'right' , 'bottom': 'bottom', 'display': 3,'index': 7}
+        {'top': 'Project 1', 'bottom':'bottom', 'left': 'left', 'right': 'right', 'display': 2,'index': 8},
+        {'top': 'Project 2', 'bottom':'bottom', 'left': 'left', 'right': 'right', 'display': 9,'index': 1}, 
+        {'top': 'Project 3', 'bottom':'bottom', 'left': 'left', 'right': 'right', 'display': 4,'index': 6}, 
+        {'top': 'Project 4', 'bottom':'bottom', 'left': 'left', 'right': 'right', 'display': 1,'index': 10},
+        {'top': 'Project 5', 'bottom':'bottom', 'left': 'left', 'right': 'right', 'display': 3,'index': 3},
+        {'top': 'Project 6', 'bottom':'bottom', 'left': 'left', 'right': 'right', 'display': 8,'index': 2}, 
+        {'top': 'Project 7', 'bottom':'bottom', 'left': 'left', 'right': 'right', 'display': 6,'index': 4}, 
+        {'top': 'Project 8', 'bottom':'bottom', 'left': 'left', 'right': 'right', 'display': 2,'index': 8}, 
+        {'top': 'Project 9', 'bottom':'bottom', 'left': 'left', 'right': 'right', 'display': 9,'index': 1},
+        {'top': 'Project 10','bottom':'bottom', 'left': 'left', 'right': 'right' , 'display': 5,'index': 5}, 
+        {'top': 'Project 11','bottom':'bottom', 'left': 'left', 'right': 'right' , 'display': 7,'index': 3}, 
+        {'top': 'Project 12','bottom':'bottom', 'left': 'left', 'right': 'right' , 'display': 3,'index': 7}
     ]
     return(
         <HeatMap 
--- src/components/HeatMapComponent.js
+++ src/components/HeatMapComponent.js
@@ -1,51 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './heatmap.css';
 
-class HeatMap extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            index: this.props.index, 
-            color: this.props.color 
+function mapGrid(props) {
+    let map = props.index.map((square) => {
+        let style = {
+            backgroundColor: `rgb(${props.color[0]}, ${props.color[1]}, ${props.color[2]}, ${square.index/10})` 
         }
-    }
-
-    mapGrid() {
-        let map = this.state.index.map((square) => {
-            let style = {
-                backgroundColor: `rgb(${this.state.color[0]}, ${this.state.color[1]}, ${this.state.color[2]}, ${square.index/10})` 
-            }
-            return (
-                <div className='grid-item' style={style}>
-                    
-                        <p className='grid-description align-middle'>
-                            {square.top}
-                            <br></br>
-                            {square.left}&nbsp; 
-                            <span className='grid-number'>
-                                {square.display}
-                            </span>
-                            &nbsp;{square.right}
-                            <br></br>
-                            <span>
-                                {square.bottom}
-                            </span>
-                            
-                        </p>
-                </div>
-            ); 
-        });
-        return map; 
-    }
-
-    render() {
-        let grid = this.mapGrid(); 
-        return(
-            <div className='grid-container'>
-                {grid}
+        return (
+            <div className='grid-item' style={style}>
+                <p className='grid-description align-middle'>
+                    {square.top}
+                    <br></br>
+                    {square.left}&nbsp; 
+                    <span className='grid-number'>
+                        {square.display}
+                    </span>
+                    &nbsp;{square.right}
+                    <br></br>
+                    <span>
+                        {square.bottom}
+                    </span>
+                </p>
             </div>
         ); 
-    }
+    });
+    return map; 
+}
+
+function HeatMap(props) {
+    let grid = mapGrid(props); 
+    return (
+        <div className='grid-container'>
+            {grid}
+        </div>
+    ); 
 }
 
 export default HeatMap; 
